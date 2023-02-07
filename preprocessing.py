74f108173c768ba4f6040852d5530659c8e57117
alberto <alberto.mancino94@gmail.com>
2023-02-07 19:18:17 +0100

    datasets

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,5 @@
-from data_preprocessing import movielens_preprocessing, facebook_book_preprocessing, yahoo_movies_preprocessing
-
-movielens_data_folder = './data/movielens'
-facebook_book_folder = './data/facebook_book'
-yahoo_movies_folder = './data/yahoo_movies'
+from data_preprocessing import alibaba_preprocessing
+alibaba_data_folder = './data/alibaba'
 
 if __name__ == '__main__':
-    facebook_book_preprocessing.run(data_folder=facebook_book_folder)
-    yahoo_movies_preprocessing.run(data_folder=yahoo_movies_folder)
-    movielens_preprocessing.run(data_folder=movielens_data_folder)
+    alibaba_preprocessing.run(data_folder=alibaba_data_folder)
