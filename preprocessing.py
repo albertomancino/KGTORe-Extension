8c52d5010f2fe7d76f0c747c6dac8e8a8b4a913c
alberto <alberto.mancino94@gmail.com>
2023-02-08 18:56:49 +0100

    datasets prefiltering

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,7 @@
-from data_preprocessing import alibaba_preprocessing
+from data_preprocessing import alibaba_preprocessing, lastfm_preprocessing
 alibaba_data_folder = './data/alibaba'
+lastfm_data_folder = './data/last_fm'
 
 if __name__ == '__main__':
     alibaba_preprocessing.run(data_folder=alibaba_data_folder)
+    lastfm_preprocessing.run(data_folder=lastfm_data_folder)
