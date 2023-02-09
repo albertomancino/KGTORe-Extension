eabc33e6a48d5d80f17a8f46dbb64bafb76ff86b
alberto <alberto.mancino94@gmail.com>
2023-02-09 12:03:54 +0100

    email sender

---
+++ email_notifier/email_sender.py
@@ -0,0 +1,114 @@
+import smtplib, ssl
+from email.message import EmailMessage
+import os
+
+NOTIFIER_FOLDER = './email_notifier'
+
+DATA_PATH = os.path.join(NOTIFIER_FOLDER, './info.txt')
+MESSAGE_PATH = os.path.join(NOTIFIER_FOLDER, 'message.txt')
+
+PORT = 465  # For SSL
+SMPT_SERVER = "smtp.gmail.com"
+
+
+def read_data(path):
+    senders = []
+    receivers = []
+    with open(path, 'r') as file:
+        for row in file.readlines():
+            tokens = row.split('\t')
+            role = tokens[0]
+            if role == 'receiver':
+                receivers.append(tokens[1].replace('\n', ''))
+            elif role == 'sender':
+                senders.append({
+                    'mail': tokens[1],
+                    'pass': tokens[2].replace('\n', '')
+                })
+    return senders, receivers
+
+
+def read_message(path):
+    messages = []
+    with open(path, 'r') as file:
+        for row in file.readlines():
+            tokens = row.split('\t')
+            role = tokens[0]
+            messages.append({
+                'role': role,
+                'subject': tokens[1],
+                'text': tokens[2].replace('\n', '')
+            })
+    return messages
+
+
+def send_email(senders=None, receivers=None, messages=None, error=False, error_exception=None):
+
+    if isinstance(senders, list):
+        pass
+    else:
+        senders_path = os.path.abspath(DATA_PATH)
+
+        if isinstance(senders, str):
+            assert os.path.exists(senders), 'senders file path does not exist'
+            senders_path = os.path.abspath(os.path.join(NOTIFIER_FOLDER, senders))
+
+        senders, _ = read_data(senders_path)
+
+    if isinstance(receivers, list):
+        pass
+    else:
+        receivers_path = os.path.abspath(DATA_PATH)
+
+        if isinstance(receivers, str):
+            assert os.path.exists(receivers), 'receivers file path does not exist'
+            receivers_path = os.path.abspath(os.path.join(NOTIFIER_FOLDER, receivers))
+
+        _, receivers = read_data(receivers_path)
+
+    if isinstance(messages, list):
+        pass
+    else:
+        messages_path = os.path.abspath(MESSAGE_PATH)
+
+        if isinstance(messages, str):
+            assert os.path.exists(messages), 'messages file path does not exist'
+            messages_path = os.path.abspath(os.path.join(NOTIFIER_FOLDER, messages))
+
+        messages = read_message(messages_path)
+
+    for sender in senders:
+        sender_mail = sender['mail']
+        sender_pass = sender['pass']
+
+        for receiver in receivers:
+
+            for message in messages:
+                role = message['role']
+                if error:
+                    if role == 'error':
+                        if error_exception:
+                            message['text'] = message['text'] + f'\nException: {error_exception}'
+                        send(sender_mail, receiver, message, sender_pass)
+                else:
+                    if role == 'all' or role == receiver:
+                        send(sender_mail, receiver, message, sender_pass)
+
+
+def send(sender, receiver, message, password, smpt_server=SMPT_SERVER, port=PORT):
+    print(f'sending message from {sender} to {receiver}')
+    context = ssl.create_default_context()
+
+    message_obj = EmailMessage()
+    message_obj['Subject'] = message['subject']
+    message_obj['From'] = sender
+    message_obj['To'] = receiver
+    message_obj.set_content(message['text'])
+
+    try:
+        with smtplib.SMTP_SSL(smpt_server, port, context=context) as server:
+            server.login(sender, password)
+            server.send_message(message_obj)
+        print(f'email from {sender} to {receiver} sent')
+    except:
+        print(f'an error occurred while sending an email from {sender} to {receiver} sent')
--- start_experiments.py
+++ start_experiments.py
@@ -1,15 +1,23 @@
 from elliot.run import run_experiment
-from email_notify import email_sender
+from email_notifier import email_sender
 import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
 parser.add_argument('--config', type=str, default='movielens_best_kgtore')
+parser.add_argument('--email', type=str, default='')
 args = parser.parse_args()
+
+email_reference = args.email
+senders, receivers, messages = None, None, None
+if email_reference:
+    senders = '_'.join([email_reference, 'info.txt'])
+    receivers = '_'.join([email_reference, 'info.txt'])
+    messages = '_'.join([email_reference, 'message.txt'])
 
 try:
     run_experiment(f"config_files/{args.config}.yml")
-    email_sender.send_email()
+    email_sender.send_email(senders=senders, receivers=receivers, messages=messages)
 except Exception as e:
-    email_sender.send_email(messages=[{'role': 'all',
-                                       'subject': 'tutto rotto',
-                                       'text': 'l\'esperimento è andato male'}])
+    print(f'An error occurred. Exception {e}')
+    print('Sending emails')
+    email_sender.send_email(senders=senders, receivers=receivers, messages=messages, error=True, error_exception=e)
